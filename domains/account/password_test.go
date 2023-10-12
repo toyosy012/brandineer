49167acc005a75a517bdd3c5f138034537d6ab53
toyosy012 <toyosy012@gmail.com>
2023-10-13 01:03:31 +0900

    feat: CREATE-PASSWORD-#2 テストにパスワードバリデーターを追加
    バリデーターの実装を定義
    
    スイートにバリデーターを追加
    NSPVによる漏洩パスワードチェックエラーを追加

--- domains/account/password_test.go
+++ domains/account/password_test.go
@@ -1,9 +1,11 @@
 package account
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
+	"github.com/s-tajima/nspv"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -12,6 +14,11 @@ const (
 	validLowerLengthPassword   = "Zj4GvQKy"
 	validUpperLengthPassword   = "EEErp7FhWXiL4fYEsKJieBWFzW55WpKPkNmAkUT9PL6vmkzEwPwD25AtMVWXMkF3jTkjN3VL"
 	invalidUpperLengthPassword = "3Z7WX4AgZY4z79HPnBdJtg2eYHL4Y9TvxScDMrvCFuFitiN679nWk8fq4zetexzPdVzLnkr4i"
+	pwnedPassword              = "12345678"
+)
+
+var (
+	errPwned = errors.New("漏洩しやすいパスワード")
 )
 
 func TestPassword(t *testing.T) {
@@ -20,9 +27,42 @@ func TestPassword(t *testing.T) {
 
 type PasswordTestSuite struct {
 	suite.Suite
+
+	validator Validator
 }
 
-func newPasswordTestSuite() *PasswordTestSuite { return &PasswordTestSuite{} }
+type NSPVValidator struct {
+	validate *nspv.Validator
+}
+
+func NewNSPVValidator() NSPVValidator {
+	v := nspv.NewValidator()
+	v.SetHibpThreshold(1)
+	return NSPVValidator{
+		validate: v,
+	}
+}
+func (v NSPVValidator) Valid(value Password) error {
+	r, err := v.validate.Validate(value.Value())
+	if err != nil {
+		return err
+	}
+
+	if r != nspv.ViolateHibpCheck {
+		return errPwned
+	}
+	return nil
+}
+
+func newPasswordTestSuite() *PasswordTestSuite {
+	return &PasswordTestSuite{
+		validator: NewNSPVValidator(),
+	}
+}
+
+func (s *PasswordTestSuite) Validator() Validator {
+	return s.validator
+}
 
 func (s *PasswordTestSuite) TestMoreLowerLimitLen() {
 	s.T().Log(
