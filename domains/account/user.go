5a40226286fa4c0fa22b4d446c299a5169cbf75e
toyosy012 <toyosy012@gmail.com>
2023-10-15 21:47:51 +0900

    feat: CREATE-PASSWORD-#2 ユーザー名
    構造体を追加
    
    ユーザー名の先頭が英数字に限定
    2文字目移行に複数の記号を許容

---
+++ domains/account/user.go
@@ -0,0 +1,47 @@
+package account
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
+
+const (
+	MinUsernameLength = 3
+	MaxUsernameLength = 100
+	UsernameFormat    = "^(\\w|\\d)+[\\w\\d-_.@+*^~¥|]{2,99}"
+)
+
+var (
+	usernameFormatRegexp = regexp.MustCompile(UsernameFormat)
+	errMinUsernameLength = errors.New(fmt.Sprintf("ユーザー名は%d文字以上で入力してください", MinUsernameLength))
+	errMaxUsernameLength = errors.New(fmt.Sprintf("ユーザー名は%d文字未満で入力してください", MaxUsernameLength))
+	errUsernameFormat    = errors.New("ユーザー名に使用できない文字が含まれています")
+)
+
+type Username string
+
+func NewUsername(value string) Username {
+	return Username(value)
+}
+func (u Username) Valid() error {
+	if len(u) < MinUsernameLength {
+		return errMinUsernameLength
+	}
+
+	if MaxUsernameLength < len(u) {
+		return errMaxUsernameLength
+	}
+
+	if !usernameFormatRegexp.MatchString(string(u)) {
+		return errUsernameFormat
+	}
+
+	return nil
+}
+
+type UserAccount struct {
+	username Username
+}
+
+func (u UserAccount) Username() Username { return u.username }
