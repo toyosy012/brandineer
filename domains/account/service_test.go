6b2722c4d8adb400f086508ceb5a8823f6c071c3
toyosy012 <toyosy012@gmail.com>
2023-10-16 23:21:04 +0900

    feat: CREATE-PASSWORD-#3 IDによるFind処理のテストを追加
    Findを実装するためのテストとセットアップを追加
    
    テストを外部非依存にしたいのでリポジトリのスタブを定義
    サービスの境界を超えるデータの入出力があるのでinput, outputを定義

--- domains/account/service_test.go
+++ domains/account/service_test.go
@@ -1,9 +1,23 @@
 package account
 
 import (
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
+
+const (
+	validServiceUUID = "f311a818-5d48-4603-8543-368976dcca6b"
+)
+
+type UserAccountRepositoryStub struct{}
+
+func NewUserAccountRepositoryStub() UserAccountRepositoryStub {
+	return UserAccountRepositoryStub{}
+}
+func (s UserAccountRepositoryStub) Find(input UserAccountInput) (*UserAccountOutput, error) {
+	return s.validUserAccountOutput, nil
+}
 
 func TestUserAccountService(t *testing.T) {
 	s := NewUserAccountServiceTestSuite()
@@ -12,6 +26,37 @@ func TestUserAccountService(t *testing.T) {
 
 type UserAccountServiceTest struct {
 	suite.Suite
+
+	id                        uuid.UUID
+	userAccountService        UserAccountService
+	validFindUserAccountInput FindUserAccountInput
+	validUserAccountOutput    UserAccountOutput
 }
 
 func NewUserAccountServiceTestSuite() UserAccountServiceTest { return UserAccountServiceTest{} }
+
+func (s *UserAccountServiceTest) SetupSuite() {
+	id, err := uuid.Parse(validServiceUUID)
+	if err != nil {
+		s.Fail(err.Error())
+		return
+	}
+	s.id = id
+	dbStub := NewUserAccountRepositoryStub()
+	s.userAccountService = NewUserAccountService(dbStub)
+}
+
+func (s *UserAccountServiceTest) TestFindUserAccount() {
+	input := NewFindUserAccountInput(s.id)
+	output, err := s.userAccountService.Find(input)
+	if err != nil {
+		s.Fail(err.Error())
+		return
+	}
+
+	account := output.UserAccount()
+	s.Equal(
+		NewUserAccount(),
+		account,
+	)
+}
