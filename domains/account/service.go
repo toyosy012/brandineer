0da7b053cd05b7f620ffbb304a1b9e6ea8291b91
toyosy012 <toyosy012@gmail.com>
2023-10-17 06:49:30 +0900

    feat: CREATE-PASSWORD-#3 ユーザーアカウントサービス追加
    Find処理でコントローラへ戻すことを想定するためOutputで戻るように定義
    リポジトリのインターフェースを定義

--- domains/account/service.go
+++ domains/account/service.go
@@ -1,1 +1,47 @@
 package account
+
+import (
+	"net/mail"
+
+	"github.com/google/uuid"
+)
+
+type UserAccountRepository interface {
+	Find(uuid.UUID) (*UserAccount, error)
+}
+
+type UserAccountOutput struct {
+	id       uuid.UUID
+	name     Username
+	email    mail.Address
+	password Password
+}
+
+func NewUserAccountOutput(account UserAccount) UserAccountOutput {
+	return UserAccountOutput{
+		id:       account.id,
+		name:     account.username,
+		email:    account.email,
+		password: account.password,
+	}
+}
+
+type UserAccountService struct {
+	userAccountRepository UserAccountRepository
+}
+
+func (s UserAccountService) Find(id uuid.UUID) (*UserAccountOutput, error) {
+	account, err := s.userAccountRepository.Find(id)
+	if err != nil {
+		return nil, err
+	}
+
+	output := NewUserAccountOutput(*account)
+	return &output, nil
+}
+
+func NewUserAccountService(repository UserAccountRepository) UserAccountService {
+	return UserAccountService{
+		userAccountRepository: repository,
+	}
+}
